b21f3c6899b7780530f2295d431b58555a2e5df6
Raissa Desyandita Tedjomurti <72061462+raissadesyandita@users.noreply.github.com>
2020-12-05 02:30:15 +0700

    Create: Header & Footer

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./About.css";
+
+
+const About = () => {
+    return (
+        <div>
+            <h1 
+                className="about-me">
+                Hi! I'm Raissa.
+            </h1>
+            <h3
+                className="about-me-">
+                A junior front-end developer who might be curious learning many new things.
+            </h3>
+        </div>
+    )
+}
+
+
+export default About;
